0278839471642b3aa3fb34c104f3a2775b22e40f
aflaster11 <a.flaster@gmail.com>
2018-06-13 19:45:32 -0600

    ProcessAdminModule & TransactionAndStorageModule

---
+++ src/DBMS_Sim/Backend/ProcessAdminModule.java
@@ -0,0 +1,66 @@
+package DBMS_Sim.Backend;
+
+import java.util.PriorityQueue;
+
+public class ProcessAdminModule implements Module{
+    private NormalDistributionGenerator distribution;
+    private boolean busyAttendant;
+    private PriorityQueue<Query> queriesInLine;
+    private int[] totalConnectionsByQueryType;
+    private double[] timeByQueryType;
+
+    @Override
+    public void processArrival(Query query) {
+
+    }
+
+    @Override
+    public void processDeparture(Query query) {
+
+    }
+
+    @Override
+    public void resetVariables() {
+
+    }
+
+    public void setDistribution(NormalDistributionGenerator distribution) {
+        this.distribution = distribution;
+    }
+
+    public void setBusyAttendant(boolean busyAttendant) {
+        this.busyAttendant = busyAttendant;
+    }
+
+    public void setQueriesInLine(PriorityQueue<Query> queriesInLine) {
+        this.queriesInLine = queriesInLine;
+    }
+
+    public void setTimeByQueryType(double[] timeByQueryType) {
+        this.timeByQueryType = timeByQueryType;
+    }
+
+    public void setTotalConnectionsByQueryType(int[] totalConnectionsByQueryType) {
+        this.totalConnectionsByQueryType = totalConnectionsByQueryType;
+    }
+
+    public NormalDistributionGenerator getDistribution() {
+        return distribution;
+    }
+
+    public boolean isBusyAttendant() {
+        return busyAttendant;
+    }
+
+    public PriorityQueue<Query> getQueriesInLine() {
+        return queriesInLine;
+    }
+
+    public double[] getTimeByQueryType() {
+        return timeByQueryType;
+    }
+
+    public int[] getTotalConnectionsByQueryType() {
+        return totalConnectionsByQueryType;
+    }
+}
---
+++ src/DBMS_Sim/Backend/TransactionAndStorageModule.java
@@ -0,0 +1,76 @@
+package DBMS_Sim.Backend;
+
+import java.util.PriorityQueue;
+
+public class TransactionAndStorageModule implements Module{
+    private int maxQueries;
+    private int busyAttendants;
+    private PriorityQueue<Query> queriesInLine;
+    private boolean ddlStatementFlag;
+    private int[] totalConnectionsByQueryType;
+    private double[] timeByQueryType;
+
+    @Override
+    public void processArrival(Query query) {
+
+    }
+
+    @Override
+    public void processDeparture(Query query) {
+
+    }
+
+    @Override
+    public void resetVariables() {
+
+    }
+
+    public void setMaxQueries(int maxQueries) {
+        this.maxQueries = maxQueries;
+    }
+
+    public void setBusyAttendants(int busyAttendants) {
+        this.busyAttendants = busyAttendants;
+    }
+
+    public void setQueriesInLine(PriorityQueue<Query> queriesInLine) {
+        this.queriesInLine = queriesInLine;
+    }
+
+    public void setDdlStatementFlag(boolean ddlStatementFlag) {
+        this.ddlStatementFlag = ddlStatementFlag;
+    }
+
+    public void setTotalConnectionsByQueryType(int[] totalConnectionsByQueryType) {
+        this.totalConnectionsByQueryType = totalConnectionsByQueryType;
+    }
+
+    public void setTimeByQueryType(double[] timeByQueryType) {
+        this.timeByQueryType = timeByQueryType;
+    }
+
+    public int getMaxQueries() {
+        return maxQueries;
+    }
+
+    public int getBusyAttendants() {
+        return busyAttendants;
+    }
+
+    public PriorityQueue<Query> getQueriesInLine() {
+        return queriesInLine;
+    }
+
+    public boolean isDdlStatementFlag() {
+        return ddlStatementFlag;
+    }
+
+    public int[] getTotalConnectionsByQueryType() {
+        return totalConnectionsByQueryType;
+    }
+
+    public double[] getTimeByQueryType() {
+        return timeByQueryType;
+    }
+    
+}
