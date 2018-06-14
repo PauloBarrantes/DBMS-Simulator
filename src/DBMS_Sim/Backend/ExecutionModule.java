b7badbd43de6803a749fe5c6845383d9ab50734c
Ducan-Ally <Fakebian98@gmail.com>
2018-06-13 19:56:55 -0600

    Added Execution Module

---
+++ src/DBMS_Sim/Backend/ExecutionModule.java
@@ -0,0 +1,71 @@
+package DBMS_Sim.Backend;
+
+import java.util.PriorityQueue;
+
+public class ExecutionModule implements Module{
+    private int maxStatements;
+    private int busyAttendants;
+    private PriorityQueue<Query> queriesInLine;
+    private int[] totalConnectionsByQueryType;
+    private double[] timeByQueryType;
+
+
+
+    @Override
+    public void processArrival(Query query){
+
+    }
+
+    @Override
+    public void processDeparture(Query query) {
+
+    }
+
+    @Override
+    public void resetVariables() {
+
+    }
+
+
+
+    public void setMaxStatements(int maxStatements) {
+        this.maxStatements = maxStatements;
+    }
+
+    public void setBusyAttendants(int busyAttendants) {
+        this.busyAttendants = busyAttendants;
+    }
+
+    public void setQueriesInLine(PriorityQueue<Query> queriesInLine) {
+        this.queriesInLine = queriesInLine;
+    }
+
+    public void setTotalConnectionsByQueryType(int[] totalConnectionsByQueryType) {
+        this.totalConnectionsByQueryType = totalConnectionsByQueryType;
+    }
+
+    public void setTimeByQueryType(double[] timeByQueryType) {
+        this.timeByQueryType = timeByQueryType;
+    }
+
+
+    public int getMaxStatements() {
+        return maxStatements;
+    }
+
+    public int getBusyAttendants() {
+        return busyAttendants;
+    }
+
+    public PriorityQueue<Query> getQueriesInLine() {
+        return queriesInLine;
+    }
+
+    public int[] getTotalConnectionsByQueryType() {
+        return totalConnectionsByQueryType;
+    }
+
+    public double[] getTimeByQueryType() {
+        return timeByQueryType;
+    }
+}
