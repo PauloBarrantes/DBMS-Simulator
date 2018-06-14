e12a236fb387795e2bdb8e0cf692dc778719270b
Ducan-Ally <Fakebian98@gmail.com>
2018-06-13 20:05:17 -0600

    Added Client Admin Module

---
+++ src/DBMS_Sim/Backend/ClientAdminModule.java
@@ -0,0 +1,107 @@
+package DBMS_Sim.Backend;
+
+import java.util.PriorityQueue;
+
+public class ClientAdminModule implements Module{
+    private int maxConnections;
+    private int busyAttendants;
+    private PriorityQueue<Query> queriesInLine;
+    private int[] totalConnectionsByQueryType;
+    private double[] timeByQueryType;
+    private double timeInTheSystem;
+    private  double timeout;
+    private int discardedConnections;
+
+    @Override
+    public void processArrival(Query query){
+
+    }
+
+    @Override
+    public void processDeparture(Query query) {
+
+    }
+
+    @Override
+    public void resetVariables() {
+
+    }
+
+    public void showResult(Query query) {
+
+    }
+
+    public void processFinalization(Query query) {
+
+    }
+
+    public boolean isItTimeout(Query query) {
+        return true;
+    }
+
+
+
+    public void setMaxConnections(int maxConnections) {
+        this.maxConnections = maxConnections;
+    }
+
+    public void setBusyAttendants(int busyAttendants) {
+        this.busyAttendants = busyAttendants;
+    }
+
+    public void setQueriesInLine(PriorityQueue<Query> queriesInLine) {
+        this.queriesInLine = queriesInLine;
+    }
+
+    public void setTotalConnectionsByQueryType(int[] totalConnectionsByQueryType) {
+        this.totalConnectionsByQueryType = totalConnectionsByQueryType;
+    }
+
+    public void setTimeByQueryType(double[] timeByQueryType) {
+        this.timeByQueryType = timeByQueryType;
+    }
+
+    public void setTimeout(double timeout) {
+        this.timeout = timeout;
+    }
+
+    public void setTimeInTheSystem(double timeInTheSystem) {
+        this.timeInTheSystem = timeInTheSystem;
+    }
+
+    public void setDiscardedConnections(int discardedConnections) {
+        this.discardedConnections = discardedConnections;
+    }
+
+    public int getMaxConnections() {
+        return maxConnections;
+    }
+
+    public int getBusyAttendants() {
+        return busyAttendants;
+    }
+
+    public PriorityQueue<Query> getQueriesInLine() {
+        return queriesInLine;
+    }
+
+    public int[] getTotalConnectionsByQueryType() {
+        return totalConnectionsByQueryType;
+    }
+
+    public double[] getTimeByQueryType() {
+        return timeByQueryType;
+    }
+
+    public double getTimeout() {
+        return timeout;
+    }
+
+    public double getTimeInTheSystem() {
+        return timeInTheSystem;
+    }
+
+    public int getDiscardedConnections() {
+        return discardedConnections;
+    }
+}
