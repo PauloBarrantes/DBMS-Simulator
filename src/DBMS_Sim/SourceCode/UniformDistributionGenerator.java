936d3758fa32b19d199fc3b52af6188d0adc3d55
aflaster11 <a.flaster@gmail.com>
2018-06-16 19:00:34 -0600

    Uniform Distribution

--- src/DBMS_Sim/SourceCode/UniformDistributionGenerator.java
+++ src/DBMS_Sim/SourceCode/UniformDistributionGenerator.java
@@ -1,29 +1,37 @@
 package DBMS_Sim.SourceCode;
 
+import java.util.Random;
+
 public class UniformDistributionGenerator implements RandomVariableGenerator{
-    private double leftBoudary;
-    private double rightBoudary;
+    private double leftBoundary;
+    private double rightBoundary;
+    private Random rnd = new Random(System.currentTimeMillis());
 
-    public UniformDistributionGenerator(){ }
 
-    public void setLeftBoudary(double leftBoudary){
-        this.leftBoudary = leftBoudary;
+    public UniformDistributionGenerator(double leftBoundary, double rightBoundary){
+        this.leftBoundary = leftBoundary;
+        this.rightBoundary = rightBoundary;
     }
 
-    public void setRightBoudary(double rightBoudary){
-        this.rightBoudary = rightBoudary;
+    public void setLeftBoundary(double leftBoundary){
+        this.leftBoundary = leftBoundary;
     }
 
-    public double getLeftBoudary(){
-        return leftBoudary;
+    public void setRightBoundary(double rightBoundary){
+        this.rightBoundary = rightBoundary;
     }
 
-    public double getRightBoudary(){
-        return rightBoudary;
+    public double getLeftBoundary(){
+        return leftBoundary;
+    }
+
+    public double getRightBoundary(){
+        return rightBoundary;
     }
 
     @Override
     public double generate() {
-        return 0;
+        double r = rnd.nextDouble();
+        return rightBoundary + (leftBoundary - rightBoundary) * r;
     }
 }
