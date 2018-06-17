7f21c08241c1f934469d44c6d1d923f1b8fdd48d
Ducan-Ally <Fakebian98@gmail.com>
2018-06-16 19:44:03 -0600

    Avances en el query generator

--- src/DBMS_Sim/SourceCode/QueryGenerator.java
+++ src/DBMS_Sim/SourceCode/QueryGenerator.java
@@ -1,6 +1,7 @@
 package DBMS_Sim.SourceCode;
 
 
+import java.util.Random;
 
 /**
  * Esta clase consiste en el cuerpo de la simulación del
@@ -14,12 +15,32 @@ package DBMS_Sim.SourceCode;
  */
 public class QueryGenerator {
     private ExpDistributionGenerator distribution;
+    private Random rnd = new Random(System.currentTimeMillis());
 
+    public QueryGenerator(){
+        //The media is 30 queries each minute, that means that in one second it recieves
+        //0.5 queries.
+        distribution = new ExpDistributionGenerator(0.5);
+    }
 
-    public QueryGenerator(){}
+    public Query generate(double clock){
+        Statement newStatement = Statement.SELECT;
+        boolean readOnly = true;
 
-    public Statement generate(){
-        Statement st = Statement.DDL;
+        double acumulated = rnd.nextDouble();
+
+        if(0.3 <= acumulated <= 0.55){
+            Statement newStatement = Statement.UPDATE;
+        }else{
+            if(0.55 < acumulated <= 0.9){
+                Statement newStatement = Statement.JOIN;
+            }else{
+                if(0.9 < acumulated <= 1.0){
+                    Statement newStatement = Statement.DDL;
+                }
+            }
+        }
+
         return st;
     }
 
