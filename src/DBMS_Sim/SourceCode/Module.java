8694d9e07408bab403eb1239f4f17b5277fd7222
Ducan-Ally <Fakebian98@gmail.com>
2018-06-18 00:01:33 -0600

    Agregado de nuevos metodos a la clase Module

--- src/DBMS_Sim/SourceCode/Module.java
+++ src/DBMS_Sim/SourceCode/Module.java
@@ -15,6 +15,10 @@ import java.util.Queue;
  */
 public class Module {
     protected final static int NUMSTATEMENTS = 4;
+    protected final static int SELECT = 0;
+    protected final static int UPDATE = 1;
+    protected final static int JOIN = 2;
+    protected final static int DDL = 3;
 
     protected int maxFields;
     protected int occupiedFields;
@@ -85,7 +89,6 @@ public class Module {
     }
 
     /**
-     *
      * @param clock, current clock time.
      * @function checks if any query in the queue needs to be removed.
      */
@@ -99,13 +102,12 @@ public class Module {
     }
 
     /**
-     *
      * @param clock, current clock time.
      * @param query, query that we wanna try to remove.
      * @return boolean that becomes true if the query was removed successfully.
      * @function checks if the query overstayed in the System, if so it removes it.
      */
-    public boolean removeQuery(double clock, Query query){
+    private boolean removeQuery(double clock, Query query){
         boolean success = false;
         if(query.elapsedTimeInSystem(clock) >= this.timeout){
             success = queriesInLine.remove(query);
@@ -113,7 +115,58 @@ public class Module {
         return success;
     }
 
+    /**
+     * @param query, the query that arrived to the Module.
+     * @function check the type of the query, and depending on which is it, increase the amount of
+     * that type of query.
+     */
+    private void countNewQuery(Query query){
+        if(query.getStatementType() == StatementType.SELECT){
+            ++totalConnectionsByQueryType[SELECT];
+        }else{
+            if(query.getStatementType() == StatementType.UPDATE){
+                ++totalConnectionsByQueryType[UPDATE];
+            }else{
+                if(query.getStatementType() == StatementType.JOIN){
+                    ++totalConnectionsByQueryType[JOIN];
+                }else{
+                    if(query.getStatementType() == StatementType.DDL){
+                        ++totalConnectionsByQueryType[DDL];
+                    }else{
+                        System.out.println("Unknown query type");
+                    }
+                }
+            }
+        }
+    }
 
+    /**
+     * @param clock, current clock time.
+     * @param query, the query that is leaving the Module.
+     * @function check the type of the query, and depending on which is it, increase the amount of
+     * that type of query is staying in the Module.
+     */
+    private void countStayedTime(double clock, Query query){
+        double stayedTime = query.elapsedTimeInModule(clock);
+
+        if(query.getStatementType() == StatementType.SELECT){
+            timeByQueryType[SELECT] += stayedTime;
+        }else{
+            if(query.getStatementType() == StatementType.UPDATE){
+                timeByQueryType[UPDATE] += stayedTime;
+            }else{
+                if(query.getStatementType() == StatementType.JOIN){
+                    timeByQueryType[JOIN] += stayedTime;
+                }else{
+                    if(query.getStatementType() == StatementType.DDL){
+                        timeByQueryType[DDL] += stayedTime;
+                    }else{
+                        System.out.println("Unknown query type");
+                    }
+                }
+            }
+        }
+    }
 
     // ---------------------------------------------------------------------------------------------
     // -------------------------------- End of the methods section --------------------------------
